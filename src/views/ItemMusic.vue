64f9385e214fbc11a594b9c706fdd600b2e7d4d6
PAULDADA <1811426355@qq.com>
2022-06-18 15:41:27 +0800

    16-推荐歌单使用onMounted钩取数据

--- src/views/ItemMusic.vue
+++ src/views/ItemMusic.vue
@@ -5,8 +5,27 @@
 </template>
 
 <script>
+import {useRoute} from 'vue-router'
+import {onMounted, reactive} from "vue";
+import {getMusicItemList} from "@/axios/api/API-ItemMusic";
+
 export default {
-  name: "ItemMusic"
+  name: "ItemMusic",
+  setup(){
+    const state = reactive({
+      playlist: {},                            //歌单的详情页的数据
+      itemList: [],                            //歌单的歌曲
+    });
+    onMounted(async ()=>{
+      let id = useRoute().query.id
+      // console.log(id)
+
+      let res = await getMusicItemList(id); // 获取歌单详情页
+      // console.log(res);
+      state.playlist = res.data.playlist;
+
+    })
+  }
 }
 </script>
 

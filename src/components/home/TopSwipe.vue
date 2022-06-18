872835a961b92d3b392019aba9febdc7afb5266a
PAULDADA <1811426355@qq.com>
2022-06-18 13:48:18 +0800

    10-项目引入axios & 使用onMounted钩取数据

--- src/components/home/TopSwipe.vue
+++ src/components/home/TopSwipe.vue
@@ -1,22 +1,32 @@
 <template>
   <div id="topSwipe">
     <van-swipe :autoplay="3000" lazy-render>
-      <van-swipe-item v-for="image in images" :key="image">
-        <img :src="image"  alt=""/>
+      <van-swipe-item v-for="image in state.images" :key="image">
+        <img :src="image.pic"  alt=""/>
       </van-swipe-item>
     </van-swipe>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { reactive,onMounted} from "vue";
+
 export default {
   name: "TopSwipe",
   setup() {
-    const images = [
-      'https://fastly.jsdelivr.net/npm/@vant/assets/apple-1.jpeg',
-      'https://fastly.jsdelivr.net/npm/@vant/assets/apple-2.jpeg',
-    ];
-    return { images };
+    const state = reactive({
+      images: [
+        'https://fastly.jsdelivr.net/npm/@vant/assets/apple-1.jpeg',
+        'https://fastly.jsdelivr.net/npm/@vant/assets/apple-2.jpeg',
+      ]
+    });
+    onMounted(()=>{
+      axios.get('http://localhost:3000/banner?type=2').then((res)=>{
+        state.images = res.data.banners
+      })
+    })
+    return { state };
   },
 }
 </script>

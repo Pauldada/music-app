7e986253731c48daedff9037142841d135bac3aa
PAULDADA <1811426355@qq.com>
2022-06-18 13:30:05 +0800

    9-引入轮播

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="home">
     <TopNav />
+    <TopSwipe/>
   </div>
 </template>
 
 <script>
 import TopNav from '@/components/home/TopNav'
+import TopSwipe from "@/components/home/TopSwipe";
 export default {
   name: 'HomeView',
   components: {
-    TopNav
+    TopNav,TopSwipe
   }
 }
 </script>

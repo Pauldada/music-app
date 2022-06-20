dabde06d21c570a7e5742daf1e09f70a7801f473
PAULDADA <1811426355@qq.com>
2022-06-20 16:15:46 +0800

    59-登录页面 & 样式

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="login">
-    login
+    <div class="loginTop">欢迎登录</div>
+    <div class="loginContent">
+      <input
+          type="text"
+          name="phone"
+          class="phone"
+          v-model="phone"
+          placeholder="请输入手机号码"
+      />
+      <input
+          type="password"
+          name="password"
+          class="password"
+          v-model="password"
+          placeholder="请输入密码"
+      />
+      <button class="btn" @click="Login">登录</button>
+    </div>
   </div>
 </template>
 
@@ -10,6 +27,39 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="less" scoped>
+.login {
+  width: 100%;
+  height: 13.34rem;
+  padding: 0.2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgb(248, 97, 97);
+  .loginTop {
+    margin-top: 1rem;
+    font-size: 1rem;
+    color: #fff;
+  }
+  .loginContent {
+    width: 100%;
+    height: 300px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    margin-top: 2rem;
+    .phone,
+    .password {
+      width: 5rem;
+      height: 1rem;
+      border: 0.02rem solid #999;
+    }
+    .btn{
+      width: 2rem;
+      height: .6rem;
+    }
+  }
+}
 </style>
+

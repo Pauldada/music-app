7a0a55b75c27768a36219bb6d6ae80a02dc2544f
PAULDADA <1811426355@qq.com>
2022-06-20 18:14:13 +0800

    65-登录页面样式

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,20 +2,24 @@
   <div class="login">
     <div class="loginTop">欢迎登录</div>
     <div class="loginContent">
-      <input
-          type="text"
-          name="phone"
-          class="phone"
-          v-model="phone"
-          placeholder="请输入手机号码"
-      />
-      <input
-          type="password"
-          name="password"
-          class="password"
-          v-model="password"
-          placeholder="请输入密码"
-      />
+      <div class="phone">
+        <input
+            type="text"
+            name="phone"
+            v-model="phone"
+            class="phone-content"
+            placeholder="请输入手机号码"
+        />
+      </div>
+      <div class="password">
+        <input
+            type="password"
+            name="password"
+            class="password-content"
+            v-model="password"
+            placeholder="请输入密码"
+        />
+      </div>
       <button class="btn" @click="Login">登录</button>
     </div>
   </div>
@@ -59,29 +63,44 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: rgb(248, 97, 97);
+  background-image: url("../assets/background4.jpg");
+  background-size:100%;
   .loginTop {
-    margin-top: 1rem;
+    margin-top: 2rem;
     font-size: 1rem;
-    color: #fff;
+    color: #ffc0cb;
   }
   .loginContent {
     width: 100%;
-    height: 300px;
+    height: 200px;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: space-around;
-    margin-top: 2rem;
+    margin-top: 1rem;
     .phone,
     .password {
       width: 5rem;
-      height: 1rem;
-      border: 0.02rem solid #999;
+      height: .8rem;
+      border: 0.06rem solid #ffc0cb;
+      border-radius: 40px;
+      .phone-content,
+      .password-content{
+        height: 100%;
+        margin-left: 30px;
+        border: none;
+        background: rgba(0, 0, 0, 0);
+      }
     }
+
     .btn{
-      width: 2rem;
-      height: .6rem;
+      width: 3rem;
+      height: .8rem;
+      border: 0.06rem solid #ffc0cb;
+      background-color: #ffc0cb;
+      color: white;
+      font-size: 18px;
+      border-radius: 20px;
     }
   }
 }

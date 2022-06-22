3ac2b02a55c2614e0ab904a413098e5a96d8f85e
PAULDADA <1811426355@qq.com>
2022-06-22 12:56:24 +0800

    72-修改API为我的服务器 & 跨域插件

--- src/axios/BaseURL.js
+++ src/axios/BaseURL.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 let service = axios.create({
-    baseURL:"http://localhost:3000",
-    timeout:3000
+    baseURL:"http://pauldada.plus:3000/",
+    // baseURL:"127.0.0.1:3000",
+    // baseURL:"https://netease-cloud-music-api-two-gray.vercel.app/",
+    timeout:10000
 })
 
 export default service
